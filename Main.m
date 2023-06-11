7bb0526bf222dec400d24e3a78c62fd8057be36b
eraserman59 <55302930+eraserman59@users.noreply.github.com>
2023-06-11 14:28:01 -0700

    Update Main.m
    Update with adjustments to have code generate dropped member graphs

--- Main.m
+++ Main.m
@@ -1,17 +1,26 @@
 clear
-Pos = [-1 0; -.4 0; 0 -.1; .5 0; 1.2 0];
-finalPos = Project(Pos);
+%Pos = [-1 0; -.4 0; 0 -.1; .5 0; 1.2 0];
+%finalPos = Project(Pos);
 
 %simulate if one droped out of the formation
 
-finalPos(4,:) = [];
-Project(finalPos)
+%finalPos(4,:) = [];
+%Project(finalPos)
 
-Pos = [-2 1; -1 1; -.5 0; 0 -1; .4 1; .8 1.5; .9 2];
-finalPos = Project(Pos);
+%Pos = [-2 1; -1 1; -.5 0; 0 -1; .4 1; .8 1.5; .9 2];
+%finalPos = Project(Pos);
 
 %simulate if one droped out of the formation
 
-finalPos(4,:) = [];
+%finalPos(4,:) = [];
 
-Project(finalPos)
+%Project(finalPos)
+
+Pos = [-3.1 2; -2.8 1.1; -2 1.05; -1 1; -.5 0; 0 -1; .4 1; .8 1.5; .9 2; 2 2; 2.3 2.1; 3 2.5];
+finalPos = Project(Pos);
+finalPos(7,:) = [];
+finalPos(6,:) = [];
+finalPos(5,:) = [];
+finalPos(4,:) = [];
+finalPos(3,:) = [];
+finalPos2 = Project(finalPos);
